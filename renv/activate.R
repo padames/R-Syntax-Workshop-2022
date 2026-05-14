14e4383c8d7e2d92b077338c2c3d2917cbea0b06
CalgaryR <pablo.adames@protonmail.com>
2026-05-14 00:34:13 -0600

    Merge debris removed and URLs added to README.md
    1) Removed unresolved merge conflict symbols from activate.R for renv to work well before running renv.activate() again to regenerate the file correctly.
    2) Added seven ShinyApp.io URLs for tutorial pages to the README

--- renv/activate.R
+++ renv/activate.R
@@ -2,12 +2,8 @@
 local({
 
   # the requested version of renv
-<<<<<<< HEAD
   version <- "1.2.2"
   attr(version, "md5") <- "bb69b6403b1bad0442657e9e8e57cc83"
-=======
-  version <- "1.1.5"
->>>>>>> 6865661c576e777447b65b9d77bc0d38a1daabaf
   attr(version, "sha") <- NULL
 
   # the project directory
@@ -173,7 +169,6 @@ local({
     if (quiet)
       return(invisible())
   
-<<<<<<< HEAD
     # also check for config environment variables that should suppress messages
     # https://github.com/rstudio/renv/issues/2214
     enabled <- Sys.getenv("RENV_CONFIG_STARTUP_QUIET", unset = NA)
@@ -184,8 +179,6 @@ local({
     if (!is.na(enabled) && tolower(enabled) %in% c("false", "0"))
       return(invisible())
   
-=======
->>>>>>> 6865661c576e777447b65b9d77bc0d38a1daabaf
     msg <- sprintf(fmt, ...)
     cat(msg, file = stdout(), sep = if (appendLF) "\n" else "")
   
@@ -233,7 +226,6 @@ local({
     section <- header(sprintf("Bootstrapping renv %s", friendly))
     catf(section)
   
-<<<<<<< HEAD
     # ensure the target library path exists; required for file.copy(..., recursive = TRUE)
     dir.create(library, showWarnings = FALSE, recursive = TRUE)
   
@@ -248,8 +240,6 @@ local({
       }
     }
   
-=======
->>>>>>> 6865661c576e777447b65b9d77bc0d38a1daabaf
     # attempt to download renv
     catf("- Downloading renv ... ", appendLF = FALSE)
     withCallingHandlers(
@@ -275,10 +265,7 @@ local({
   
     # add empty line to break up bootstrapping from normal output
     catf("")
-<<<<<<< HEAD
-=======
-  
->>>>>>> 6865661c576e777447b65b9d77bc0d38a1daabaf
+
     return(invisible())
   }
   
@@ -295,7 +282,6 @@ local({
     repos <- Sys.getenv("RENV_CONFIG_REPOS_OVERRIDE", unset = NA)
     if (!is.na(repos)) {
   
-<<<<<<< HEAD
       # split on ';' if present
       parts <- strsplit(repos, ";", fixed = TRUE)[[1L]]
   
@@ -310,14 +296,7 @@ local({
         names(vals) <- "CRAN"
   
       return(vals)
-=======
-      # check for RSPM; if set, use a fallback repository for renv
-      rspm <- Sys.getenv("RSPM", unset = NA)
-      if (identical(rspm, repos))
-        repos <- c(RSPM = rspm, CRAN = cran)
-  
-      return(repos)
->>>>>>> 6865661c576e777447b65b9d77bc0d38a1daabaf
+
   
     }
   
@@ -566,7 +545,6 @@ local({
   
   }
   
-<<<<<<< HEAD
   renv_bootstrap_find <- function(version) {
   
     path <- renv_bootstrap_find_cache(version)
@@ -612,8 +590,6 @@ local({
   
   }
   
-=======
->>>>>>> 6865661c576e777447b65b9d77bc0d38a1daabaf
   renv_bootstrap_download_tarball <- function(version) {
   
     # if the user has provided the path to a tarball via
@@ -1082,11 +1058,9 @@ local({
   
   renv_bootstrap_validate_version_release <- function(version, description) {
     expected <- description[["Version"]]
-<<<<<<< HEAD
+
     is.character(expected) && identical(c(expected), c(version))
-=======
-    is.character(expected) && identical(expected, version)
->>>>>>> 6865661c576e777447b65b9d77bc0d38a1daabaf
+
   }
   
   renv_bootstrap_hash_text <- function(text) {
@@ -1265,7 +1239,7 @@ local({
   }
   
   renv_bootstrap_run <- function(project, libpath, version) {
-<<<<<<< HEAD
+
     tryCatch(
       renv_bootstrap_run_impl(project, libpath, version),
       error = function(e) {
@@ -1281,8 +1255,7 @@ local({
   }
   
   renv_bootstrap_run_impl <- function(project, libpath, version) {
-=======
->>>>>>> 6865661c576e777447b65b9d77bc0d38a1daabaf
+
   
     # perform bootstrap
     bootstrap(version, libpath)
@@ -1305,8 +1278,7 @@ local({
     warning(paste(msg, collapse = "\n"), call. = FALSE)
   
   }
-  
-<<<<<<< HEAD
+
   renv_bootstrap_cache_version <- function() {
     # NOTE: users should normally not override the cache version;
     # this is provided just to make testing easier
@@ -1319,8 +1291,6 @@ local({
     paste("v", number - 1L, sep = "")
   }
   
-=======
->>>>>>> 6865661c576e777447b65b9d77bc0d38a1daabaf
   renv_json_read <- function(file = NULL, text = NULL) {
   
     jlerr <- NULL
